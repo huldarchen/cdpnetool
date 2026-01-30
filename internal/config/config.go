cbca2ddd772e766b8d0dcd16e43b52663a8016b5
241x <hemailads@163.com>
2026-01-30 22:45:19 +0800

    chore(version): 更新版本号至v1.1.1
    - 修改Windows安装脚本中的版本号定义
    - 更新前端package.json及package-lock.json版本号
    - 更新前端程序界面显示的版本号
    - 变更内部配置文件中的默认版本号
    - 调整wails.json中的产品版本信息

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,7 +16,7 @@ type Config struct {
 // NewConfig 创建默认配置
 func NewConfig() *Config {
 	return &Config{
-		Version: "1.0.0",
+		Version: "1.1.1",
 		Sqlite: struct {
 			Db     string `yaml:"db"`
 			Prefix string `yaml:"prefix"`
